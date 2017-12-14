902aa4878e729a9f28a8ae463cb36987e44b192d
QuentinFernandez <33118597+QuentinFernandez@users.noreply.github.com>
2017-12-14 09:58:38 +0100

    Add files via upload
    Ajout class Abstract danger + classe Loup et méthodes associées + Méthode personnage pour afficher caractéristiques (pas encore utilisée dans le main)

---
+++ projet_java-master/Jeu_Java/src/Danger.java
@@ -0,0 +1,13 @@
+
+abstract class Danger {
+	// Classe mère de deux classes : Loup et une autre à définir
+	//Caractéristiques
+	protected int vie;
+	protected int force;
+	
+	public abstract int attaquer(int p_force);
+	public abstract boolean pouvoir();
+
+
+}
+
